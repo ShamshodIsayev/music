1de3ce31d2c301cfb6170309d7a7e57cf3504d84
Shamshod <abdushamshod@gmail.com>
2022-05-03 13:06:08 +0500

    refactor: some changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Lists from "./pages/Lists";
-import SongPage from "./pages/SongPage";
 import { useDispatch } from "react-redux";
 import Home from "./pages/Home";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [src, setSrc] = useState(`./audio/`);
-  const dispatch = useDispatch();
+  useEffect(() => {
+    document.title = "Uzbek Classic Music";
+  });
 
   return (
     <div className="App">
-      {/* <Lists /> */}
-      {/* <SongPage /> */}
       <Home />
     </div>
   );
